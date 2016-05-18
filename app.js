393bb7277896c3500f62ca381b052804ae2b13a8
Sky Chen <skychen@yahoo-inc.com>
2016-05-18 11:14:13 -0700

    add binary search

--- app.js
+++ app.js
@@ -1,1 +1,5 @@
+
+console.log('\n\n*******************************');
+console.log('usingHashTableSeparateChaining');
+console.log('*******************************\n');
 var usingHashTableSeparateChaining = require('./usingHashTableSeparateChaining');
