342f837116f60a453004b6a035bc98144414a53f
Sky Chen <skychen@yahoo-inc.com>
2016-09-29 11:40:47 -0700

    restructure, add eslint

--- app.js
+++
@@ -1,5 +0,0 @@
-
-console.log('\n\n*******************************');
-console.log('usingHashTableSeparateChaining');
-console.log('*******************************\n');
-var usingHashTableSeparateChaining = require('./usingHashTableSeparateChaining');
--- usingBitUtils.js
+++
@@ -1,8 +0,0 @@
-module.exports = function usingBitUtils() {
-    var BitUtils = require('./lib/BitUtils');
-    var bitUtils = new BitUtils();
-    console.log('5 get bit 1 is ' + bitUtils.getBit(5,1));
-
-    console.log('5 set bit 1 is ' + bitUtils.setBit(5,1));
-
-}()
