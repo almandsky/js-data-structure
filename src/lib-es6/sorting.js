84d3312d91dda3f29eda407990a0fdac1e06fde6
Sky Chen <skychen@yahoo-inc.com>
2016-11-08 02:02:37 -0800

    feat(sorting): add merge sort

--- src/lib-es6/sorting.js
+++ src/lib-es6/sorting.js
@@ -2,6 +2,14 @@
 
 class Sorting {
 
+  /**
+   * bubble sort
+   * Complexity: Θ(n^2)
+   * Space Complexity: O(1)
+   * @method bubbleSort
+   * @param {Array} array - input array
+   * @return {Array} sorted array
+   */
   static bubbleSort(array) {
     if (!array || !array.length) {
       return array;
@@ -18,6 +26,14 @@ class Sorting {
     return array;
   }
 
+  /**
+   * selection sort
+   * Complexity: Θ(n^2)
+   * Space Complexity: O(1)
+   * @method selectionSort
+   * @param {Array} array - input array
+   * @return {Array} sorted array
+   */
   static selectionSort(array) {
     if (!array || !array.length) {
       return array;
@@ -39,6 +55,14 @@ class Sorting {
     return array;
   }
 
+  /**
+   * insertion sort
+   * Complexity: Θ(n^2)
+   * Space Complexity: O(1)
+   * @method insertionSort
+   * @param {Array} array - input array
+   * @return {Array} sorted array
+   */
   static insertionSort(array) {
     if (!array || !array.length) {
       return array;
@@ -55,6 +79,61 @@ class Sorting {
       array[j] = temp;
     }
     return array;
+  }
+
+  /**
+   * merge sort
+   * Complexity: O(n log n)
+   * Space Complexity: O(n)
+   * @method mergeSort
+   * @param {Array} array - input array
+   * @return {Array} sorted array
+   */
+  static mergeSort(array) {
+    if (!array || !array.length || array.length === 1) {
+      return array;
+    }
+    const length = array.length;
+    const mid = Math.floor(length / 2);
+    const left = array.slice(0, mid);
+    const right = array.slice(mid, length);
+
+    return this.merge(this.mergeSort(left), this.mergeSort(right));
+  }
+
+  /**
+   * merge two arrays, and return a new shorted array
+   * @method merge
+   * @param {Array} left - first half of the array
+   * @param {Array} right - second half of the array
+   * @return {Array} sorted array
+   */
+  static merge(left, right) {
+    const result = [];
+    let il = 0;
+    let ir = 0;
+
+    while (il < left.length && ir < right.length) {
+      if (left[il] < right[ir]) {
+        result.push(left[il]);
+        il += 1;
+      } else {
+        result.push(right[ir]);
+        ir += 1;
+      }
+    }
+
+    while (il < left.length) {
+      result.push(left[il]);
+      il += 1;
+    }
+
+    while (ir < right.length) {
+      result.push(right[ir]);
+      ir += 1;
+    }
+
+    return result;
   }
 
 }
--- test/sorting.test.js
+++ test/sorting.test.js
@@ -3,16 +3,7 @@ import Sorting from '../src/lib-es6/sorting';
 import { testArray1, testArray2, sortedArray } from './sorting.mock';
 
 const expect = require('chai').expect;
-/*
-const testArray1 = [5, 4, 3, 2, 1];
-const testArray2 = [1, 2, 3, 5, 4];
 
-const sortedArray = [1, 2, 3, 4, 5];
-
-console.log(testArray1);
-console.log(testArray2);
-console.log(sortedArray);
-*/
 describe('Sorting Test', () => {
   it('should bubble sort an array', () => {
     expect(Sorting.bubbleSort(undefined)).to.deep.equal(undefined);
@@ -30,5 +21,11 @@ describe('Sorting Test', () => {
     expect(Sorting.insertionSort(undefined)).to.deep.equal(undefined);
     expect(Sorting.insertionSort(testArray1.concat())).to.deep.equal(sortedArray);
     expect(Sorting.insertionSort(testArray2.concat())).to.deep.equal(sortedArray);
+  });
+
+  it('should merge sort an array', () => {
+    expect(Sorting.mergeSort(undefined)).to.deep.equal(undefined);
+    expect(Sorting.mergeSort(testArray1.concat())).to.deep.equal(sortedArray);
+    expect(Sorting.mergeSort(testArray2.concat())).to.deep.equal(sortedArray);
   });
 });
